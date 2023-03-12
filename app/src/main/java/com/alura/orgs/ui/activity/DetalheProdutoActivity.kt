410cecff22acfe27b08e5faab3a35c7e00a7b156
Vanderlei O de Melo <vanderleimelo10@gmail.com>
2023-03-12 12:33:26 -0300

    Construção da tela de detalhe do produto

---
+++ app/src/main/java/com/alura/orgs/ui/activity/DetalheProdutoActivity.kt
@@ -0,0 +1,17 @@
+package com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alura.orgs.R
+
+class DetalheProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_produto)
+        title =  "Detalhes"
+
+
+
+
+    }
+}
